cfec6e9f34288bd396eb24119dc8dbf90a717940
Dan Ghita <dan.ghita@softwire.com>
2017-09-23 21:15:16 +0100

    Add more validation to fill and validate method

--- SetAssociativeCache/SetAssociativeCache.Test/AssociativeCache/AssociativeCacheUnitTests.cs
+++ SetAssociativeCache/SetAssociativeCache.Test/AssociativeCache/AssociativeCacheUnitTests.cs
@@ -38,6 +38,9 @@ namespace SetAssociativeCache.Test
                 m_cache.Add(i, i.ToString());
 
             Assert.Equal(m_cacheSize, m_cache.Size);
+
+            for (int i = 0; i < m_cacheSize; ++i)
+                Assert.Equal(i.ToString(), m_cache.Get(i));
         }
 
         protected int m_cacheSize = 5;
