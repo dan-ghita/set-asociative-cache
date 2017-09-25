f742e6df240dfff126e40a937e2e15f4a7fe65e8
Dan Ghita <daghita@microsoft.com>
2017-09-25 17:38:03 +0100

    Move random eviction set associative cache tests to base and register them for all associative caches

--- SetAssociativeCache/SetAssociativeCache.Test/SetAssociativeCache/SetAssociativeCacheUnitTests.cs
+++ SetAssociativeCache/SetAssociativeCache.Test/SetAssociativeCache/SetAssociativeCacheUnitTests.cs
@@ -1,4 +1,6 @@
+using Moq;
 using SetAssociativeCache.Shared;
+using System.Collections;
 using Xunit;
 
 namespace SetAssociativeCache.Test.SetAssociativeCache
@@ -20,7 +22,55 @@ namespace SetAssociativeCache.Test.SetAssociativeCache
 
             Assert.Equal(value, m_cache.Get(key));
         }
-        
+
+        [Fact]
+        public void AddToOneWayCache_SimilarSetBits_ReplacesOldValue()
+        {
+            Mock<IBitConverter> bitConverterMock = new Mock<IBitConverter>();
+            bitConverterMock.Setup(bc => bc.ObjectToBits("1")).Returns(new BitArray(new[] { true, false, false, true, false }));
+            bitConverterMock.Setup(bc => bc.ObjectToBits("2")).Returns(new BitArray(new[] { true, false, false, true, true }));
+
+            RegisterCacheWithMock(bitConverterMock);
+
+            string key1 = "1";
+            string key2 = "2";
+            string value1 = "value1";
+            string value2 = "value2";
+
+            m_cache.Add(key1, value1);
+            Assert.Equal(value1, m_cache.Get(key1));
+
+            m_cache.Add(key2, value2);
+            Assert.Equal(value2, m_cache.Get(key2));
+
+            Assert.Equal(1, m_cache.Size);
+            Assert.Equal(null, m_cache.Get(key1));
+        }
+
+        [Fact]
+        public void AddToOneWayCache_DifferentSetBits_DoesNotReplaceOldValue()
+        {
+            Mock<IBitConverter> bitConverterMock = new Mock<IBitConverter>();
+            bitConverterMock.Setup(bc => bc.ObjectToBits("1")).Returns(new BitArray(new[] { true, false, false, true, false }));
+            bitConverterMock.Setup(bc => bc.ObjectToBits("2")).Returns(new BitArray(new[] { true, true, false, true, true }));
+            bitConverterMock.Setup(bc => bc.ConvertToInt(It.IsAny<BitArray>())).Returns<BitArray>(b => (new BitConverter()).ConvertToInt(b));
+
+            RegisterCacheWithMock(bitConverterMock);
+
+            string key1 = "1";
+            string key2 = "2";
+            string value1 = "value1";
+            string value2 = "value2";
+
+            m_cache.Add(key1, value1);
+            m_cache.Add(key2, value2);
+
+            Assert.Equal(2, m_cache.Size);
+            Assert.Equal(value1, m_cache.Get(key1));
+            Assert.Equal(value2, m_cache.Get(key2));
+        }
+
+        public abstract void RegisterCacheWithMock(Mock<IBitConverter> bitConverter);
 
         protected int m_cacheSizeInKb = 64;
 
--- SetAssociativeCache/SetAssociativeCache.Test/SetAssociativeCache/SetAssociativeMRUCacheUnitTests.cs
+++ SetAssociativeCache/SetAssociativeCache.Test/SetAssociativeCache/SetAssociativeMRUCacheUnitTests.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using Moq;
+using SetAssociativeCache.Shared;
 using Xunit;
 
 namespace SetAssociativeCache.Test.SetAssociativeCache
@@ -10,6 +12,13 @@ namespace SetAssociativeCache.Test.SetAssociativeCache
         {
             m_cache = new SetAssociativeCache<string, string>(m_cacheSizeInKb, m_numberOfWays,
                 (size) => new MRUAssociativeCache<string>(size), m_bitConverter);
+        }
+
+        public override void RegisterCacheWithMock(Mock<IBitConverter> bitConverterMock)
+        {
+            // 1 * 1024 / 64 = 16 -> first 4 bit for set index
+            ISetAssociativeCache<string, string> m_cache = new SetAssociativeCache<string, string>(1, 1,
+                (size) => new MRUAssociativeCache<string>(size), bitConverterMock.Object);
         }
     }
 }
